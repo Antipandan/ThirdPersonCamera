c8d59539ebfca3bb2d0d7563b91cb4867fd9fe61
Antipandan <jacob@blota.se>
2026-04-13 19:21:58 +0200

    basic version of thirdperson camera working!

--- Assets/Scripts/MouseLookController.cs
+++ Assets/Scripts/MouseLookController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,27 +7,31 @@ using UnityEngine.InputSystem;
 [Serializable]
 public class MouseLookController : MonoBehaviour, IPauseable
 {
+    [SerializeField]
     private Transform head;
+    
+    [SerializeField]
     private Transform body;
-    private InputAction lookAction;
-    private bool isPaused;
-
+    
     [SerializeField]
     private float mouseSpeed = 1.0f;
 
     [SerializeField, Range(0, 180)]
     private float verticalAngle = 160;
     
+    private InputAction lookAction;
+    private bool isPaused;
+    
     public bool IsPaused => isPaused;
 
     private void Start()
     {
-        head = GetComponentInChildren<Camera>().transform;
-        body = transform;
+        if (head == null) head = gameObject.GetComponentInChildren<Camera>().transform;
+        if (body == null) body = gameObject.transform;
         lookAction = InputSystem.actions.FindAction("Look");
-        
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Pause()
     {
         // xor
@@ -47,11 +52,13 @@ public class MouseLookController : MonoBehaviour, IPauseable
         float hor = look.x;
         float ver = look.y;
 
-        if (Mathf.Abs(hor) > float.Epsilon) {
+        if (Mathf.Abs(hor) > float.Epsilon) 
+        {
             body.Rotate(Vector3.up, hor * mouseSpeed);
         }
 
-        if (Mathf.Abs(ver) > float.Epsilon) {
+        if (Mathf.Abs(ver) > float.Epsilon) 
+        {
             Quaternion rot = head.localRotation;
             Quaternion aim = Quaternion.AngleAxis(-0.5f * verticalAngle * Mathf.Sign(ver), Vector3.right);
             Quaternion delta = Quaternion.RotateTowards(rot, aim, Mathf.Sign(ver) * ver * mouseSpeed);
