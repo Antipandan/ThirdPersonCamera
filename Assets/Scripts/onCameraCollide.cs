b58a73fa324fbbac6589b7254212baa9e6655344
Antipandan <jacob@blota.se>
2026-04-16 16:12:12 +0200

    remove debug stuff and comments

--- Assets/Scripts/onCameraCollide.cs
+++ Assets/Scripts/onCameraCollide.cs
@@ -13,8 +13,6 @@ public class onCameraCollide : MonoBehaviour
     private float distanceCameraPlayer;
     private Vector3 oldHeadTransformLocalPos;
     private Quaternion oldHeadTransformRotation;
-    private Vector3 oldCameraPosition;
-    private Quaternion oldCameraRotation;
     
     private void Awake()
     {
@@ -36,11 +34,8 @@ public class onCameraCollide : MonoBehaviour
             float distanceX = player.position.x - gameObject.transform.position.x;
             float distanceY = player.position.y - gameObject.transform.position.y;
             float distanceZ = player.position.z - gameObject.transform.position.z;
-            // Debug.Log($"currentDistance: {Mathf.Sqrt(distanceX * distanceX + distanceY * distanceY)}");
-            // ge lite extra distance
             if (Mathf.Sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ) > distanceCameraPlayer + 1/50f)
             {
-                // TODO detta är problematiskt när spelare hoppar upp och ned framför en väg
                 headTransform.SetParent(player);
                 isFollowing = true;
             }
@@ -48,21 +43,12 @@ public class onCameraCollide : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        Vector3 CameraPosition = gameObject.transform.position;
-        Vector3 PlayerPosition = player.position;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (canBeTriggered)
         {
-            Debug.Log($"trigger entered!");
             oldHeadTransformLocalPos = headTransform.localPosition;
             oldHeadTransformRotation = headTransform.localRotation;
-            oldCameraPosition = gameObject.transform.localPosition;
-            oldCameraRotation = gameObject.transform.localRotation;
             headTransform.SetParent(null);
             isFollowing = false;
             canBeTriggered = false;
@@ -71,26 +57,15 @@ public class onCameraCollide : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log($"distance between player and camera: {distanceCameraPlayer}");
         headTransform.SetParent(player.transform, false);
-        // testar lite nya saker!
-        //SetLocalCameraPosition(oldCameraRotation, oldCameraPosition, gameObject.GetComponent<Camera>());
         SetLocalHeadRotation(oldHeadTransformRotation, oldHeadTransformLocalPos, headTransform);
         canBeTriggered = true;
     }
 
-    private static void SetLocalCameraPosition(Quaternion rotation,Vector3 oldLocalPosition, Camera camera)
-    {
-        camera.gameObject.transform.localRotation = rotation;
-        camera.gameObject.transform.localPosition = oldLocalPosition;
-    }
-
     private static void SetLocalHeadRotation(Quaternion rotation, Vector3 localPosition, Transform head)
     {
-        Debug.Log($"localRotation: {rotation}, localPosition: {localPosition}");
         head.localRotation = rotation;
         head.localPosition = localPosition;
-        Debug.Log($"new values LocalRotation: {head.localRotation}, localPosition: {head.localPosition}");
     }
     
 }
